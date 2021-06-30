2ec0f6f36ef62c3333d9e53119d1d7315e36e4eb
ShanJiang1911 <johnson33399@gmail.com>
2021-06-30 13:48:28 -0700

    add AboutMe and change NavBar name

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,12 +1,60 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import NavBarClass from './NavBarClass';
 
 
 class AboutUs extends React.Component {
   render () {
     return (
-      <h1>About Us</h1> 
+      <>
+        <NavBarClass/>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Qaalid Hashi</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Jona Brown</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Alex Vogt</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="picture/shanImg.jpg" />
+          <Card.Body>
+            <Card.Title>Shan Jiang</Card.Title>
+            <Card.Text>
+              Hi my name is Shan. Do you want some noodle? 
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </>
     )
   }
 }
 
 export default AboutUs;
+
+
---
+++ src/NavBarClass.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+class NavBarClass extends React.Component {
+  render () {
+    return (
+      <>
+        <Navbar/>
+      </>
+    )
+  }
+}
+
+
+export default NavBarClass;
