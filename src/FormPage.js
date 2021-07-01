c61b5127e000b4438e4aff701d6f0413ba4a181f
Alex Vogt <alexvogt18@gmail.com>
2021-06-30 20:25:09 -0700

    Fixes bugs from yesterday, CSS additions

--- src/FormPage.js
+++ src/FormPage.js
@@ -30,7 +30,7 @@ handleGetRecipes = async () => {
   let searchData = this.state.ingredients.join(' ');
   console.log(searchData);
   let recipeData = await axios.get(`${REACT_APP_PORT}/searchIngredients?ingredients=${searchData}`);
-    let dataToSave = recipeData.data.slice(0,15);
+    let dataToSave = recipeData.data.slice(0,8);
     console.log(recipeData);
     this.setState({recipes: dataToSave})
 }
@@ -48,42 +48,40 @@ handleGetRecipes = async () => {
     console.log(this.state.ingredients);
     return (
       <>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group controlId='enterMeat'>
-            <Form.Label>Meat</Form.Label>
-            <Form.Control type="text" placeholder="Enter Meat"/>
-          </Form.Group>
+        <div class = 'form'>
+          <Form class= 'form' onSubmit={this.handleSubmit}>
+            <Form.Group controlId='enterMeat'>
+              <Form.Label>Meat</Form.Label>
+              <Form.Control type="text" placeholder="Enter Meat"/>
+            </Form.Group>
 
-          <Form.Group controlId='enterDairy'>
-            <Form.Label>Dairy</Form.Label>
-            <Form.Control type="string" placeholder="Enter Dairy Products"/>
-          </Form.Group>
+            <Form.Group controlId='enterDairy'>
+              <Form.Label>Dairy</Form.Label>
+              <Form.Control type="string" placeholder="Enter Dairy Products"/>
+            </Form.Group>
 
-          <Form.Group controlId='enterVeggies'>
-            <Form.Label>Veggies</Form.Label>
-            <Form.Control type="string" placeholder="Enter Veggies"/>
-          </Form.Group>
+            <Form.Group controlId='enterVeggies'>
+              <Form.Label>Veggies</Form.Label>
+              <Form.Control type="string" placeholder="Enter Veggies"/>
+            </Form.Group>
 
-          <Form.Group controlId='enterSpices'>
-            <Form.Label>Spices</Form.Label>
-            <Form.Control type="string" placeholder="Enter Spice"/>
-          </Form.Group>
+            <Form.Group controlId='enterSpices'>
+              <Form.Label>Spices</Form.Label>
+              <Form.Control type="string" placeholder="Enter Spice"/>
+            </Form.Group>
 
-          <Button variant="primary" type="submit" >Submit</Button>
-        </Form>
-        
-        <div>
-        {this.state.ingredients ? this.state.ingredients.map((item, idx) => {
-          console.log('This item is: ' + item, typeof item);
-          if(item.length > 0) {return <Toast key={idx} onClose = {() => this.toggleShow(idx)}><Toast.Header>{item}</Toast.Header></Toast>}else{return ''};
+            <Button variant="primary" type="submit" >Submit</Button>
+          </Form>
+          {this.state.ingredients ? this.state.ingredients.map((item, idx) => {
+          if(item.length > 0) {return <Toast class='toast' key={idx} onClose = {() => this.toggleShow(idx)}><Toast.Header class= 'toastheader'>{item}</Toast.Header></Toast>}else{return ''};
         }): ''}
         </div>
-        <div>
-        {this.state.ingredients.length > 0 ? <Button onClick={this.handleGetRecipes}>Get Recipes!</Button> : ''}
-        </div>
+        <div class= 'results'>
+        {this.state.ingredients.length > 0 ? <Button onClick={this.handleGetRecipes} size= "lg" block>Get Recipes!</Button> : ''}
         {this.state.recipes.length > 0 ? this.state.recipes.map((recipe, idx) => {
           console.log(recipe);
           return <CardDeck class = 'cardDeck'><RecipeCard class = 'recipe' key={idx} recipeData = {recipe} /></CardDeck>}) : ''}
+        </div>  
       </>
     )
   }
