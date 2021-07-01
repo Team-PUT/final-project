842d4c50b1f6a476265aae1a5ada0000aa10ec43
Qaalid Hashi <q.hashi16@gmail.com>
2021-07-01 12:00:41 -0700

    q-day04

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -3,16 +3,19 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './RecipeCard.css';
 
+
 class RecipeCard extends React.Component {
+
+    
     render() {
         return (
             <Card class = 'card'>
-                {this.props.recipeData.image.length > 0 ? <Card.Img class = 'card-image' variant="top" src={this.props.recipeData.image} /> : <Card.Img variant="top" src='./imagenotfound.png' />}
+                <Card.Img class = 'card-image' variant="top" src={this.props.recipeData.image} />
                 <Card.Body>
-                    <Card.Title>{this.props.recipeData.name}</Card.Title>
-                    <Card.Text>
+                    <Card.Title className = 'cardTitle'>{this.props.recipeData.name}</Card.Title>
+                    {/* <Card.Text>
                         {this.props.recipeData.source}
-                    </Card.Text>
+                    </Card.Text> */}
                         <Button class = 'cardButton' variant="primary" href={this.props.recipeData.link}>Check out this recipe!</Button>
                         <Button class = 'cardButton' variant='info'>Save!</Button>
                 </Card.Body>
