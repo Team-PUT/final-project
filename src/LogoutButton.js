6429ab4e08a13a20b1aa06f072b312a660511c00
Jona <jbrown6@berklee.edu>
2021-07-01 11:01:01 -0700

    Working profile, working navbar

--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from 'react-bootstrap';
 
+import './LogoutButton.css';
+
 const LogoutButton = () =>{
   const { logout } = useAuth0();
 
   return(
-    <Button onClick ={()=>logout({returnTo: window.location.origin})} variant="info">
+    <Button className="button" onClick ={()=>logout({returnTo: window.location.origin})}>
       Log Out
     </Button>
   );
